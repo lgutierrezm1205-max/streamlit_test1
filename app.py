898fba5743c2a1c26605407c00b85f49e2402f25
lgutierrezm1205-max <lgutierrezm1205@gmail.com>
2025-10-03 21:05:31 -0500

    initial commit

--- app.py
+++ app.py
@@ -2,14 +2,9 @@ import streamlit as st
 import pandas as pd
 import scipy
 
-st.title("Mi primera app con Streamlit")
 
-st.write("Hola, mundo!")
+import streamlit as st
 
-# Ejemplo con pandas
-df = pd.DataFrame({
-    'Columna 1': [1, 2, 3],
-    'Columna 2': [4, 5, 6]
-})
+st.header('Lanzar una moneda')
 
-st.write(df)
+st.write('Esta aplicación aún no es funcional. En construcción.')
